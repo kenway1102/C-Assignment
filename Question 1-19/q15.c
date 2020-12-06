d58f1c2b3a3aea1d8b30f57675ebad05c6171966
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:34:31 +0530

    Update q15.c

--- Question 1-19/q15.c
+++ Question 1-19/q15.c
@@ -1,1 +1,14 @@
+. #include <stdio.h>
+int main() {
+	int dec,oct=0,mul=1;
+	printf("Enter number in decimal: ");
+	scanf("%d",&dec);
+	while (dec>0) {
+		oct+=(dec%8)*mul;
+		dec/=8;
+		mul*=10;
+	}
+	printf("The number in octal is: %d",oct);
+	return 0;
+}
 
