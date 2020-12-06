e107b5c8465ceed2ae08a9790c62eacd6f78e5fc
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:17:43 +0530

    Create q7.c

---
+++ q7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+	int num,ocomp=0,mul=1,x,flag=0;
+	printf("Enter binary number: ");
+	scanf("%d",&num);
+	while (num>0) {
+		x=num%10;
+		if (flag==0) {
+			if (x==1)
+				flag=1;
+			ocomp+=x*mul;
+			mul*=10;
+			num/=10;
+			continue;
+		}
+		num/=10;
+		x+=(x==0)?1:-1;
+		ocomp+=x*mul;
+		mul*=10;
+	}
+	printf("The two's complement is: %d",ocomp);
+	return 0;
+}
