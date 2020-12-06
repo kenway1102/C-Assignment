5e5f188661f30b1f7f702559ede080d4adfc6eee
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:38:25 +0530

    Update q19.c

--- Question 1-19/q19.c
+++ Question 1-19/q19.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+	char hex[20];
+	int dec=0,len,mul=1;
+	printf("Enter hexadecimal value: ");
+	fgets(hex,20,stdin);
+	len = strlen(hex);
+	for(int i=len-2;i>=0;i--) {
+		if (hex[i]>='A' && hex[i]<='F')
+			dec+=(hex[i]-55)*mul;
+		else 
+			dec+=(hex[i]-48)*mul;
+		mul*=16;
+	}
+	printf("The number in decimal is: %d",dec);
+	return 0;
+}
 
