21f4e576122942b0e183e0907dea40c145edbdb3
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:33:59 +0530

    Update q14.c

--- Question 1-19/q14.c
+++ Question 1-19/q14.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main() {
+	int dec,bin=0,mul=1;
+	printf("Enter number in decimal: ");
+	scanf("%d",&dec);
+	while (dec>0) {
+		bin+=(dec%2)*mul;
+		dec/=2;
+		mul*=10;
+	}
+	printf("The number in binary is: %d",bin);
+	return 0;
+}
 
