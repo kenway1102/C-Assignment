8cc321af9a02d273c2568d4768c02f3cf2077a59
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:34:54 +0530

    Update q16.c

--- Question 1-19/q16.c
+++ Question 1-19/q16.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main() {
+	int dec,i=0,x,len=0;
+	char hex[20]={0},hex_rev[20]={0};
+	printf("Enter number in decimal: ");
+	scanf("%d",&dec);
+	while (dec>0) {
+		x=dec%16;
+		if (x>9)
+			hex_rev[i]=x+55;
+		else
+			hex_rev[i]=x+48;
+		i++;
+		dec/=16;
+		len++;
+	}
+	for(i=0;i<len;i++)
+		hex[i]=hex_rev[len-i-1];
+	printf("The number in hexadecimal is: %s",hex);
+	return 0;
+}
 
