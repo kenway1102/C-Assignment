97c0aee39286baaaf02ad917f684628d8aa4379d
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:29:13 +0530

    Update q6.c

--- Question 1-19/q6.c
+++ Question 1-19/q6.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
+	int num,ocomp=0,mul=1,x;
+	printf("Enter binary number: ");
+	scanf("%d",&num);
+	while (num>0) {
+		x=num%10;
+		num/=10;
+		x+=(x==0)?1:-1;
+		ocomp+=x*mul;
+		mul*=10;
+	}
+	printf("The one's complement is: %d",ocomp);
+	return 0;
+}
 
