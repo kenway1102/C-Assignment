e7691843a841ac6e997aed53a58ad614924358d6
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:33:32 +0530

    Update q13.c

--- Question 1-19/q13.c
+++ Question 1-19/q13.c
@@ -1,1 +1,27 @@
+ #include <stdio.h>
+int main() {
+	int oct,dec=0,mul=1,i=0,x,len=0;
+	char hex[20]={0},hex_rev[20]={0};
+	printf("Enter octal number: ");
+	scanf("%d",&oct);
+	while (oct>0) {
+		dec+=(oct%10)*mul;
+		oct/=10;
+		mul*=8;
+	}
+	while (dec>0) {
+		x=dec%16;
+		if (x>9)
+			hex_rev[i]=x+55;
+		else
+			hex_rev[i]=x+48;
+		i++;
+		dec/=16;
+		len++;
+	}
+	for(i=0;i<len;i++)
+		hex[i]=hex_rev[len-i-1];
+	printf("The number in hexadecimal is: %s",hex);
+	return 0;
+}
 
