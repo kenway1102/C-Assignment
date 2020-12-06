dcf08b983261c1a837fe30f0559d6fd9298e1388
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:30:28 +0530

    Update q8.c

--- Question 1-19/q8.c
+++ Question 1-19/q8.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+	int bin,oct_rev=0,dec=0,rem,mul=1;
+	printf("Enter number in binary: ");
+	scanf("%d",&bin);
+	while (bin>0) {
+		rem=bin%1000;
+		while (rem>0) {
+			dec+=(rem%10)*mul;
+			rem/=10;
+			mul*=2;
+		}
+		mul=1;
+		oct_rev=oct_rev*10+dec;
+		dec=0;
+		bin/=1000;
+	}
+	int temp=oct_rev,oct=0;
+	while (temp>0) {
+		oct=oct*10+temp%10;
+		temp/=10;
+	}
+	printf("The octal value is: %d",oct);
+	return 0;
+}
 
