d26d0d17eef6cc7e0325f91ab9809fbacbb7b26b
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:26:57 +0530

    Update q1.c

--- Question 1-19/q1.c
+++ Question 1-19/q1.c
@@ -1,1 +1,13 @@
+ #include <stdio.h>
+int main() {
+	int num,dig=0;
+	printf("Enter the number: ");
+	scanf("%d",&num);
+	while (num>0) {
+		dig++;
+		num/=10;
+	}
+	printf("There are %d digits in this number.", dig);
+	return 0;
+}
 
