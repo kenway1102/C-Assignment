c6039a30c80d1fe90c656c5c9694c7653726a37c
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-06 22:27:48 +0530

    Update q3.c

--- Question 1-19/q3.c
+++ Question 1-19/q3.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main() {
+	int freq[10]={0},num;
+	printf("Enter number: ");
+	scanf("%d",&num);
+	while (num>0) {
+		freq[num%10]++;
+		num/=10;
+	}
+	for(int i=0;i<=9;i++) 
+		printf("%d :\t%d\n",i,freq[i]);
+	return 0;
+}
 
