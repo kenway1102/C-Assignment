a95b0c798315d64ecf519879d789f1fc44cd2931
Jeteish Pratap Singh <72212719+kenway1102@users.noreply.github.com>
2020-12-07 18:17:35 +0530

    Create inv-pyramid.c

---
+++ star-pattern/inv-pyramid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+int r, i, j,k;
+   printf("Enter the number of rows: ");
+   scanf("%d", &r);
+   for (i = r; i >= 1; --i) 
+   {
+   for (k = 0; k < r - i; ++k)
+   {printf("  ");
+   }
+      for (j = i; j <= 2 * i - 1; ++j)
+      {
+         printf("* ");
+      }
+      for (j = 0; j < i - 1; ++j)
+      {
+         printf("* ");
+      }
+      printf("\n");  
+   }
+    return 0;
+}
